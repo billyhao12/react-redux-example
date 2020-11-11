9ea34d3cb494441f6cf007be7f63bf1cf5b970d8
Billy Hao <billyhao12@gmail.com>
2020-11-10 21:32:41 -0800

    Connected store to react

--- src/containers/Articles.js
+++ src/containers/Articles.js
@@ -1,25 +1,28 @@
-import React, { useState } from 'react';
-import Article from '../components/Article/Article';
-import AddArticle from '../components/AddArticle/AddArticle';
+import React from "react"
+import { connect } from "react-redux"
 
-const Articles = () => {
-    const [articles, setArticles] = useState([
-        { id: 1, title: 'post 1', body: 'Quisque cursus, metus vitae pharetra' },
-        { id: 2, title: 'post 2', body: 'Quisque cursus, metus vitae pharetra' }
-    ])
-    const saveArticle = e => {
-        e.preventDefault()
-        // the logic will be updated later
-    }
+import Article from "../components/Article/Article"
+import AddArticle from "../components/AddArticle/AddArticle"
 
-    return (
-        <div>
-            <AddArticle saveArticle={saveArticle} />
-            {articles.map(article => (
-                <Article key={article.id} article={article} />
-            ))}
-        </div>
-    )
+const Articles = ({ articles }) => {
+  const saveArticle = e => {
+    e.preventDefault()
+    // the logic will be updated later
+  }
+  return (
+    <div>
+      <AddArticle saveArticle={saveArticle} />
+      {articles.map(article => (
+        <Article key={article.id} article={article} />
+      ))}
+    </div>
+  )
 }
 
-export default Articles;
+const mapStateToProps = state => {
+  return {
+    articles: state.articles,
+  }
+}
+
+export default connect(mapStateToProps)(Articles)
